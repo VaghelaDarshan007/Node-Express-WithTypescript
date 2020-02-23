994b264731192162d265674a6c5e3f88e69188cc
VaghelaDarshan007 <vagheladarshan085@gmail.com>
2020-02-24 02:22:20 +0530

    add CORS

--- app.ts
+++ app.ts
@@ -10,6 +10,19 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 // convert input* data into json format: you can enter data in json format
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    // used for give access of API to particular user/url
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    if(req.method === 'OPTIONS'){
+        // tell browser that which method it has access for this API
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+        res.writeHead(200);
+        res.end();
+    }
+    // must be here, if not then flow will be blocked.
+    next();
+});
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 
--- dist/app.js
+++ dist/app.js
@@ -7,8 +7,21 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
-// convert data into json format
+// convert input* data into json format: you can enter data in json format
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    // used for give access of API to particular user/url
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    if (req.method === 'OPTIONS') {
+        // tell browser that which method it has access for this API
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+        res.writeHead(200);
+        res.end();
+    }
+    // must be here, if not then flow will be blocked.
+    next();
+});
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 app.use((req, res, next) => {
